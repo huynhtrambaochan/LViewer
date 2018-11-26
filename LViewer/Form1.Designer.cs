567d7a876f35e97cae692302207de4ee0be2f9e8
Le Quoc Thang <thanglequo1912@gmail.com>
2018-11-26 22:56:29 +0700

    Initial commit

---
+++ LViewer/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace LViewer
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Main));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yourProfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inviteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createGroupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutLViewerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkForUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button_Sender = new System.Windows.Forms.Button();
+            this.textBox_Input = new System.Windows.Forms.TextBox();
+            this.listBox_Status = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionToolStripMenuItem,
+            this.extrasToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1170, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectionToolStripMenuItem
+            // 
+            this.connectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yourProfileToolStripMenuItem,
+            this.inviteToolStripMenuItem,
+            this.createGroupToolStripMenuItem});
+            this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
+            this.connectionToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.connectionToolStripMenuItem.Text = "Connection";
+            // 
+            // yourProfileToolStripMenuItem
+            // 
+            this.yourProfileToolStripMenuItem.Name = "yourProfileToolStripMenuItem";
+            this.yourProfileToolStripMenuItem.Size = new System.Drawing.Size(172, 26);
+            this.yourProfileToolStripMenuItem.Text = "Your profile";
+            // 
+            // inviteToolStripMenuItem
+            // 
+            this.inviteToolStripMenuItem.Name = "inviteToolStripMenuItem";
+            this.inviteToolStripMenuItem.Size = new System.Drawing.Size(172, 26);
+            this.inviteToolStripMenuItem.Text = "Invite";
+            // 
+            // createGroupToolStripMenuItem
+            // 
+            this.createGroupToolStripMenuItem.Name = "createGroupToolStripMenuItem";
+            this.createGroupToolStripMenuItem.Size = new System.Drawing.Size(172, 26);
+            this.createGroupToolStripMenuItem.Text = "Create Group";
+            // 
+            // extrasToolStripMenuItem
+            // 
+            this.extrasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.extrasToolStripMenuItem.Name = "extrasToolStripMenuItem";
+            this.extrasToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.extrasToolStripMenuItem.Text = "Extras";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(136, 26);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutLViewerToolStripMenuItem,
+            this.checkForUpdatesToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutLViewerToolStripMenuItem
+            // 
+            this.aboutLViewerToolStripMenuItem.Name = "aboutLViewerToolStripMenuItem";
+            this.aboutLViewerToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
+            this.aboutLViewerToolStripMenuItem.Text = "About LViewer";
+            // 
+            // checkForUpdatesToolStripMenuItem
+            // 
+            this.checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            this.checkForUpdatesToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
+            this.checkForUpdatesToolStripMenuItem.Text = "Check for updates";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(54, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 98);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(7, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(204, 35);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 168);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 35);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button_Sender
+            // 
+            this.button_Sender.Location = new System.Drawing.Point(1056, 394);
+            this.button_Sender.Name = "button_Sender";
+            this.button_Sender.Size = new System.Drawing.Size(89, 40);
+            this.button_Sender.TabIndex = 7;
+            this.button_Sender.Text = "Gửi";
+            this.button_Sender.UseVisualStyleBackColor = true;
+            this.button_Sender.Click += new System.EventHandler(this.button_Sender_Click);
+            // 
+            // textBox_Input
+            // 
+            this.textBox_Input.Location = new System.Drawing.Point(324, 403);
+            this.textBox_Input.Name = "textBox_Input";
+            this.textBox_Input.Size = new System.Drawing.Size(705, 22);
+            this.textBox_Input.TabIndex = 9;
+            this.textBox_Input.TextChanged += new System.EventHandler(this.textBox_Input_TextChanged);
+            // 
+            // listBox_Status
+            // 
+            this.listBox_Status.FormattingEnabled = true;
+            this.listBox_Status.ItemHeight = 16;
+            this.listBox_Status.Location = new System.Drawing.Point(287, 32);
+            this.listBox_Status.Name = "listBox_Status";
+            this.listBox_Status.Size = new System.Drawing.Size(750, 340);
+            this.listBox_Status.TabIndex = 10;
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1170, 526);
+            this.Controls.Add(this.listBox_Status);
+            this.Controls.Add(this.textBox_Input);
+            this.Controls.Add(this.button_Sender);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form_Main";
+            this.Text = "LViewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yourProfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inviteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createGroupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutLViewerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkForUpdatesToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button_Sender;
+        private System.Windows.Forms.TextBox textBox_Input;
+        private System.Windows.Forms.ListBox listBox_Status;
+    }
+}
+
---
+++ LViewer_Client/Form_Main.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LViewer_Client
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Status = new System.Windows.Forms.TextBox();
+            this.listBox_Online = new System.Windows.Forms.ListBox();
+            this.textBox_Input = new System.Windows.Forms.TextBox();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.button_UpdateList = new System.Windows.Forms.Button();
+            this.button_PublicChat = new System.Windows.Forms.Button();
+            this.label_Username = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Status
+            // 
+            this.textBox_Status.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.textBox_Status.AllowDrop = true;
+            this.textBox_Status.Location = new System.Drawing.Point(37, 64);
+            this.textBox_Status.Multiline = true;
+            this.textBox_Status.Name = "textBox_Status";
+            this.textBox_Status.Size = new System.Drawing.Size(388, 242);
+            this.textBox_Status.TabIndex = 0;
+            // 
+            // listBox_Online
+            // 
+            this.listBox_Online.FormattingEnabled = true;
+            this.listBox_Online.ItemHeight = 16;
+            this.listBox_Online.Location = new System.Drawing.Point(517, 39);
+            this.listBox_Online.Name = "listBox_Online";
+            this.listBox_Online.Size = new System.Drawing.Size(250, 260);
+            this.listBox_Online.TabIndex = 1;
+            // 
+            // textBox_Input
+            // 
+            this.textBox_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Input.Location = new System.Drawing.Point(36, 347);
+            this.textBox_Input.Name = "textBox_Input";
+            this.textBox_Input.Size = new System.Drawing.Size(312, 27);
+            this.textBox_Input.TabIndex = 2;
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(363, 344);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(84, 32);
+            this.button_Send.TabIndex = 3;
+            this.button_Send.Text = "Gửi";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // button_UpdateList
+            // 
+            this.button_UpdateList.Location = new System.Drawing.Point(517, 321);
+            this.button_UpdateList.Name = "button_UpdateList";
+            this.button_UpdateList.Size = new System.Drawing.Size(185, 28);
+            this.button_UpdateList.TabIndex = 4;
+            this.button_UpdateList.Text = "Refresh";
+            this.button_UpdateList.UseVisualStyleBackColor = true;
+            this.button_UpdateList.Click += new System.EventHandler(this.button_UpdateList_Click);
+            // 
+            // button_PublicChat
+            // 
+            this.button_PublicChat.Location = new System.Drawing.Point(517, 377);
+            this.button_PublicChat.Name = "button_PublicChat";
+            this.button_PublicChat.Size = new System.Drawing.Size(185, 29);
+            this.button_PublicChat.TabIndex = 5;
+            this.button_PublicChat.Text = "Public Chat";
+            this.button_PublicChat.UseVisualStyleBackColor = true;
+            this.button_PublicChat.Click += new System.EventHandler(this.button_PublicChat_Click);
+            // 
+            // label_Username
+            // 
+            this.label_Username.AutoSize = true;
+            this.label_Username.Location = new System.Drawing.Point(37, 13);
+            this.label_Username.Name = "label_Username";
+            this.label_Username.Size = new System.Drawing.Size(46, 17);
+            this.label_Username.TabIndex = 6;
+            this.label_Username.Text = "label1";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_Username);
+            this.Controls.Add(this.button_PublicChat);
+            this.Controls.Add(this.button_UpdateList);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.textBox_Input);
+            this.Controls.Add(this.listBox_Online);
+            this.Controls.Add(this.textBox_Status);
+            this.Name = "Form_Main";
+            this.Text = "LViewer";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Status;
+        private System.Windows.Forms.ListBox listBox_Online;
+        private System.Windows.Forms.TextBox textBox_Input;
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.Button button_UpdateList;
+        private System.Windows.Forms.Button button_PublicChat;
+        private System.Windows.Forms.Label label_Username;
+    }
+}
